669424ac158f7e37d3973cdccce19733ca45117b
FinnS013 <92523249+FinnS013@users.noreply.github.com>
2023-01-23 09:55:45 +0000

    table products ingevuld en begonnen table platform invullen

--- db/create.sql
+++ db/create.sql
@@ -12,6 +12,7 @@ CREATE TABLE products (
 
 CREATE TABLE platform (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name_game VARCHAR(255),
   Windows boolean,
   Linux boolean,
   MacOS boolean,
@@ -21,17 +22,17 @@ CREATE TABLE platform (
 
 CREATE TABLE genre (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  naam_genre TEXT
+  naam_genre VARCHAR(255)
 );
 
 CREATE TABLE fysiek_digitaal (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  fysiek_digitaal TEXT
+  fysiek_digitaal VARCHAR(255)
 );
 
 CREATE TABLE leeftijd_categorie (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  leeftijd_categorie TEXT
+  leeftijd_categorie VARCHAR(255)
 );
 
 --
@@ -43,10 +44,22 @@ CREATE TABLE leeftijd_categorie (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Fortnite', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('Post Mortem', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('Scarlet and the Black, The', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+--
+-- table products
+--
+insert into products (name, description, code, price) values ('Fortnite: Save the World', 'description', '816905633-0', 20);
+insert into products (name, description, code, price) values ('Minecrat: Java Edition', 'description', '077030122-3', 30);
+insert into products (name, description, code, price) values ('Call of Duty', 'description', '445924201-X', 60);
+insert into products (name, description, code, price) values ('Factorio', 'description', '693155505-7', 30);
+insert into products (name, description, code, price) values ('For Honor', 'description', '686928463-6', 30);
+insert into products (name, description, code, price) values ('Dishonored', 'description', '492662523-7', 20);
+insert into products (name, description, code, price) values ('Hollow Knight', 'description', '492662523-8', 25);
+insert into products (name, description, code, price) values ('Ori and The Blind Forest', 'description', '492662523-9', 20);
+insert into products (name, description, code, price) values ('Dirt Rally', 'description', '492662523-4', 20);
+insert into products (name, description, code, price) values ('Doom Eternal', 'description', '492662523-5', 60);
 
+--
+-- table platform
+--
+insert into platform (name_game, Windows, Linux, MacOS, PS4, XBOX) values ('Fortnite: Save the world', TRUE, TRUE, TRUE, TRUE, TRUE);
+insert into platform (name_game, Windows, Linux, MacOS, PS4, XBOX) values ('Minecraft', TRUE, TRUE, TRUE, FALSE, FALSE);
